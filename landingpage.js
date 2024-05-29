3b0fb807b4fe85a227d12fcfc0b6d138bb30a013
Samuel Adeyemo <crownprincesamuelaadeyemo@gmail.com>
2024-05-29 13:17:20 +0100

    done 3

--- landingpage.js
+++ landingpage.js
@@ -2,13 +2,13 @@ var toggleMenu = () =>{
     var nav_container = document.getElementById("navlist-container");
     var nav_icon = document.getElementById("nav-icon");
 
-    if (nav_container.style.display === "none") {
-        //nav_icon.classList.remove("fa-navicon")
-        nav_icon.classList.replace("fa-navicon", "fa-close")
-        nav_container.style.display = "block";
-    }
-    else{
+    if (nav_container.style.display === "block") {
         nav_container.style.display = "none";
         nav_icon.classList.replace("fa-close", "fa-navicon")
+    }
+    else{
+        nav_icon.classList.replace("fa-navicon", "fa-close")
+        nav_container.style.display = "block";
+        
     }
 }
